dbf0ae8227e214babbc787688297c615a2e5ac38
Yuri Vyatkin <yurivyatkin@users.noreply.github.com>
2023-03-06 11:25:25 +1300

    Add ability to create topic and view the list

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -33,5 +33,45 @@ const Content: React.FC = () => {
     }
   );
 
-  return <div>{JSON.stringify(topics)}</div>
+  const createTopic =  api.topic.create.useMutation({
+    onSuccess() {
+      void refetchTopics();
+    },
+  })
+
+  return (
+    <div className="mx-5 mt-5 grid grid-cols-4 gap-2">
+      <div className="px-2">
+        <ul className="menu rounded-box w-56 bg-base-100 p-2">
+          {topics?.map((topic) => (
+            <li key={topic.id}>
+              <a
+                href="#"
+                onClick={(evt) => {
+                  evt.preventDefault();
+                }}
+              >
+               {topic.title}
+              </a>
+            </li>
+          ))}          
+        </ul>
+        <div className="divider"></div>
+        <input
+          type="text"
+          placeholder="New Topic"
+          className="input-bordered input input-sm w-full"
+          onKeyDown={(e) => {
+            if (e.key === "Enter") {
+              createTopic.mutate({
+                title: e.currentTarget.value,
+              });
+              e.currentTarget.value = "";
+            }
+          }}
+        />
+      </div>
+      <div className="col-span-3"></div>
+    </div>
+  );
 }
