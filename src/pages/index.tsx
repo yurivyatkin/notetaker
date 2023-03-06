65dddb4e1a6440a4709084e553355046cf2872bb
Yuri Vyatkin <yurivyatkin@users.noreply.github.com>
2023-03-06 16:18:33 +1300

    Make notes list UI with delete button

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,6 +6,7 @@ import { useSession } from "next-auth/react";
 import { api, type RouterOutputs } from "../utils/api";
 import { Header } from "~/components/Header";
 import { NoteEditor } from "~/components/NoteEditor";
+import { NoteCard } from "~/components/NoteCard";
 
 const Home: NextPage = () => {
   return (
@@ -63,6 +64,12 @@ const Content: React.FC = () => {
     },
   });
 
+  const deleteNote = api.note.delete.useMutation({
+    onSuccess: () => {
+      void refetcNotes();
+    },
+  });
+
   return (
     <div className="mx-5 mt-5 grid grid-cols-4 gap-2">
       <div className="px-2">
@@ -97,6 +104,17 @@ const Content: React.FC = () => {
         />
       </div>
       <div className="col-span-3">
+        <div>
+          {notes?.map((note) => (
+            <div key={note.id} className="mt-5">
+              <NoteCard
+                note={note}
+                onDelete={() => void deleteNote.mutate({ id: note.id })}
+              />
+            </div>
+          ))}
+        </div>
+
         <NoteEditor
           onSave={({ title, content }) => {
             void createNote.mutate({
